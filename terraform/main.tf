00b06c738b67754e982e58704c81bbedb28e8937
Romi Jung <romij@google.com>
2026-08-27 11:45:05 +0000

    fix(terraform): correct local.services reference in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ locals {
 }
 
 resource "google_project_service" "enabled_apis" {
-  for_each                   = toset(locals.services)
+  for_each                   = toset(local.services)
   project                    = var.project_id
   service                    = each.key
   disable_on_destroy         = false
